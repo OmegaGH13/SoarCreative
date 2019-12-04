f25d30760e0ad8c822f26f5b01efda6782bc0305
Omar Gharandoq <omar.gharandoq@ad.infosys.com>
2019-12-04 15:06:17 -0600

    Added Structure to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { BookComponent } from './book/book.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ClientViewComponent } from './client-view/client-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BookComponent,
+    ContactComponent,
+    LoginComponent,
+    ClientViewComponent
   ],
   imports: [
     BrowserModule
